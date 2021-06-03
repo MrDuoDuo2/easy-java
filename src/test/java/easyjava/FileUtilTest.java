651456524b4d04001529c292b27e9e813c153106
mrduoduo2 <nmbh1122@126.com>
2021-06-03 18:04:32 +0800

    update

--- src/test/java/easyjava/FileUtilTest.java
+++ src/test/java/easyjava/FileUtilTest.java
@@ -10,7 +10,7 @@ import java.nio.charset.StandardCharsets;
 public class FileUtilTest {
     @Test
     public void writeToLocal() throws IOException {
-        String filePath = "/home/zyx/text.txt"; //文件在target目录下
+        String filePath = "../text.txt"; //文件在target目录下
         String content = "hello";
         InputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
         FilUtil.writeToLocal(filePath,inputStream);
